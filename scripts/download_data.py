c5cfb7a0da18927e3b78b8f6bfca1ad4736cd241
moka <54906285+moka-co@users.noreply.github.com>
2026-01-29 17:09:10 +0100

    download semeval and eltea + cleanup

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -5,14 +5,23 @@ from tqdm import tqdm
 import requests
 import os
 import zipfile
+import shutil
 
-filepath="data/"
+# Create a header to mimic a real browser
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+}
+
 
 # Download the glove model as zip, if not already present, then unzip i
 def download_glove():
+    filepath="data/glove/"
     glove_url="http://nlp.stanford.edu/data/glove.6B.zip"
     glove_filepath = os.path.join(filepath, "glove.6B.zip")
     glove_model = os.path.join(filepath, "glove.6B.300d.txt")
+
+    if not os.path.exists(filepath):
+        os.makedirs(filepath)
 
     if not os.path.exists(glove_filepath):
         try:
@@ -48,5 +57,78 @@ def download_glove():
     print(f"Successfully download and extracted Glove model under {filepath} directory")
 
 
+def download_semeval_dataset():
+    filepath = "data/datasets"
+    semeval_filepath_zip = os.path.join(filepath, "SemEval2018-Task1-all-data.zip")
+    semeval_url = "https://saifmohammad.com/WebDocs/AIT-2018/AIT2018-DATA/SemEval2018-Task1-all-data.zip"
+
+    sem_eval_path_train = os.path.join(filepath, "SemEval2018-Task1-all-data/English/E-c/2018-E-c-En-train.txt")
+    sem_eval_path_val = os.path.join(filepath, "SemEval2018-Task1-all-data/English/E-c/2018-E-c-En-test-gold.txt")
+
+    check_file = os.path.join(filepath, "2018-E-c-En-train.txt")
+    if os.path.exists(check_file):
+        print(f"Semeval dataset already present under {filepath}")
+        return
+
+    # Create filepath if it doesn't exists
+    if not os.path.exists(filepath):
+        os.makedirs(filepath)
+
+    # Create dataset file path if it does not exists
+    if not os.path.exists(semeval_filepath_zip):
+        try:
+            response = requests.get(semeval_url, headers=headers, stream=True, timeout=10)
+            response.raise_for_status()  # raise for bad status
+
+            # Save zip file
+            with open(semeval_filepath_zip, 'wb') as f:
+                for chunk in response.iter_content(chunk_size=1024):
+                    size = f.write(chunk)
+
+            # Extract zip file
+            with zipfile.ZipFile(semeval_filepath_zip,'r') as zip_ref:
+                zip_ref.extractall(filepath)
+
+        except requests.exceptions.RequestException as e:
+            print(f"Download failed: {e}")
+
+        # Remove unecessary noise
+        shutil.copy2(sem_eval_path_train, filepath)
+        shutil.copy2(sem_eval_path_val, filepath)
+        try:
+            trash_dir = os.path.join(filepath, "__MACOSX")
+            shutil.rmtree(trash_dir)
+            trash_dir = os.path.join(filepath, "SemEval2018-Task1-all-data")
+            shutil.rmtree(trash_dir)
+            os.remove(semeval_filepath_zip)
+            
+        except Exception as e:
+            print(f"Error {e} during cleanup")
+
+
+    print(f"Successfully downloaded Semeval Dataset under {filepath}")
+
+
+def download_eltea17():
+    eltea_url = "https://raw.githubusercontent.com/RoozbehBandpey/ELTEA17/refs/heads/main/datasets/train.txt"
+    filepath ="data/datasets"
+    eltea_filepath = os.path.join(filepath, "eltea_train.txt")
+
+    try:
+        response = requests.get(eltea_url, headers=headers, stream=True, timeout=10)
+        response.raise_for_status()  # raise for bad status
+
+        with open(eltea_filepath, 'wb') as f:
+            f.write(response.content)
+
+    except requests.exceptions.RequestException as e:
+        print(f"Download failed: {e}")
+
+    print(f"Successfully downloaded ELTEA17 Dataset under {filepath}")
+
+
+
 # Run functions
 download_glove()
+download_semeval_dataset()
+download_eltea17()
