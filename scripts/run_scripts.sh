8f43c9f43216753dea7eecb59e28e28d87b7a30a
moka <54906285+moka-co@users.noreply.github.com>
2026-01-31 17:10:25 +0100

    Update run_scripts.sh

--- scripts/run_scripts.sh
+++ scripts/run_scripts.sh
@@ -4,5 +4,5 @@ python scripts/download_data.py
 
 python scripts/merge_data.py
 
-python -m scripts/remove_outliers
-python -m scripts/make_dictionary
+python -m scripts.remove_outliers
+python -m scripts.make_dictionary
