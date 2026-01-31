255faaaeec5b0a9888d4f4f5f266735269970b7b
moka <54906285+moka-co@users.noreply.github.com>
2026-01-31 16:50:40 +0100

    fixed bug

--- scripts/run_scripts.sh
+++ scripts/run_scripts.sh
@@ -4,6 +4,5 @@ python scripts/download_data.py
 
 python scripts/merge_data.py
 
-python -m scripts/remove_outliers.py
-
-python -m scripts/make_dictionary.py
+python -m scripts/remove_outliers
+python -m scripts/make_dictionary
